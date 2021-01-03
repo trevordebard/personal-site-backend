0e62b80e6b8561b5deaf2584c43002b6427370ce
Trevor Debardeleben <trevordebard@gmail.com>
2021-01-03 14:12:41 -0500

    Setup postgres

--- config/database.js
+++ config/database.js
@@ -4,11 +4,19 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', ''),
+        username: env('DATABASE_USERNAME', 'trevordebardeleben'),
+        password: env('DATABASE_PASSWORD', ''),
+        schema: env('DATABASE_SCHEMA', 'public'), // Not Required
+        // ssl: {
+        //   rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+        // },
       },
       options: {
-        useNullAsDefault: true,
+        // ssl: env.bool('DATABASE_SSL', false),
       },
     },
   },
